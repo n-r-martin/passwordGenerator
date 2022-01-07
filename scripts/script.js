a1071694f492dcb984311c49a8aa6acdafac04ac
Nick Martin <martin.nicholas.r@gmail.com>
2022-01-06 20:03:26 -0700

    basic generator working... now need to allow user to select length

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,24 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  console.log('generate button clicked');
 
-  passwordText.value = password;
+  var password = generatePassword(5);
+
+    function generatePassword(length) {
+      let result = ' ';
+      const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const charactersLength = characters.length;
+      for ( let i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      }
+      return result;
+  }
+
+console.log(generatePassword(5));
+
+let passwordText = document.querySelector("#password");
+passwordText.value = password;
 
 }
 
