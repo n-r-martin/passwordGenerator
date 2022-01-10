38563df9f5b54914df151fcbf62d871657eef8f4
Nick Martin <martin.nicholas.r@gmail.com>
2022-01-09 23:28:18 -0700

    fixed bug where password generation was firing twice

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 const generateBtn = document.querySelector("#generate");
 
 // Add event listener to 'Generate' button and run the password functions
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -11,11 +11,8 @@ function writePassword() {
   //Stores value of character count slider from UI into a variable that is used in calculating the generated password length
   let passwordLength = document.querySelector("#characterCountSlider").value;
 
-  //Declaring the variable that will eventually store the generated password and pass it to the DOM as rendered text
-  let password = generatePassword(passwordLength);
-
-  // The lion's share of the work... this is where we generate a password of random chracters with help from the UI
-  function generatePassword(length) {
+  //Declaring the variable as a function that will eventually store the generated password and pass it to the DOM as rendered text
+  let generatedPassword = (function () {
       let result = ' ';
       const characters ='abcdefghijklmnopqrstuvwxyz';
       const uppercaseCharacters = characters.toUpperCase();
@@ -34,28 +31,32 @@ function writePassword() {
       const numbersChecked = document.getElementById('numbers-toggle').checked;
       const specCharChecked = document.getElementById('special-char-toggle').checked;
 
-      // Storing the variables in an array that will be looped thorough to populate the availableCharacters array
+      // Storing the variables in an array that will be looped through to populate the availableCharacters array
       let checkboxArray = [lowercaseChecked, uppercaseChecked, numbersChecked, specCharChecked];
 
-
+      
       //If all checkboxes in the checkboxArray are false, alert the user that at least one criteria (one checkbox) must be selected
-      //If at least one is true, then loops through all the checkboxes for as many as there are in the DOM
-      //Then checks if the current checkbox being evaluated is indeed checked
-      //If so, it pulls from the static resourceArray whose indexing mirrors that of the checkboxArray
-      //It then pushes that stored string to the availableCharacters array, from which the generated password gets it input
-      //Lastly, if the checkbox is unchecked, it does not push the stored string to availableChracters and logs it to the console as excluded
       if (checkboxArray.every( e => e === false)) {
         alert('Cannot generate password! Must select at least one criteria.');
-      } else {
+      }
+
+      //If at least one is true, then loops through all the checkboxes for as many as there are in the DOM, and then checks if the current checkbox being evaluated is indeed checked
+      else {
         for (let i = 0; i < checkboxArray.length; i++) {
+
+          // If checkbox being evaluated is checked, it pulls from the static resourceArray whose indexing mirrors that of the checkboxArray
           if (checkboxArray[i] == true ) {
+
+            //It then pushes that stored string to the availableCharacters array, from which the generated password gets it input
             availableCharacters.push(resourceArray[i]);  
-          } else {
+          } 
+          
+          //Lastly, if the checkbox is unchecked, it does not push the stored string to availableChracters and logs it to the console as excluded
+          else {
               console.log('Excluded ' + resourceArray[i]);
           }
         }
       }
-      
       
       // Log the current state of the availableChracters array to the console
       console.log(availableCharacters);
@@ -64,25 +65,24 @@ function writePassword() {
       let generatorInput = availableCharacters.join("");
 
       // Loop through the concatenated string and select characters at random as many times as 'length' which is set by the UI and passed to the script
-      for ( let i = 0; i < length; i++ ) {
+      for ( let i = 0; i < passwordLength; i++ ) {
           result += generatorInput.charAt(Math.floor(Math.random() * generatorInput.length));
       }
 
       // Return the randomized string - our final product - back to the function
       return result;
-  }
+  })();
 
 // Log to the console our generated password
-console.log(`Password successfully generated: ${generatePassword(passwordLength)}`);
+console.log(`Password successfully generated: ${generatedPassword}`);
 
 let passwordText = document.querySelector("#password");
 
 // Render to the UI the generated password
-passwordText.value = password;
-// DONE!!
+passwordText.value = generatedPassword;
 }
 
-
+// Copy to the clipboard functionality
 // Function to copy the value of the field to the clipboard
 function copyPassword() {
   /* Get the text field */
@@ -102,4 +102,4 @@ function copyPassword() {
    });
 }
 
-document.getElementById("copy-button").addEventListener('click', copyPassword)
+document.getElementById("copy").addEventListener('click', copyPassword);
